09b271b59c3b269d20cbf25db0edeeb2be9a5e3a
Muhammed Salih Gümüş <gumusmuhammedsalih@gmail.com>
2024-10-26 00:11:29 +0300

    create qrcode input component

---
+++ components/QRCodeInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <UFormGroup label="What would you like to encode?">
+    <UTextarea
+      :model-value="modelValue"
+      @update:model-value="$emit('update:modelValue', $event)"
+      :rows="3"
+      placeholder="Enter text, URL, contact info, or anything you'd like to turn into a QR code"
+      class="w-full"
+    />
+  </UFormGroup>
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+});
+
+defineEmits(['update:modelValue']);
+</script>
